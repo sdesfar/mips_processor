fffe91e9e2a5964b372bcf9bebac9053ae8fbcc9
Robert Jarzmik <robert.jarzmik@free.fr>
2016-12-06 17:21:28 +0100

    cpu_def: kill the state machines
    Remove the state machines and use casual integer instead.
    
    At the same time, shrink the number of signal for register index from 32
    to 6, as we suppose we'll have less than 64 registers. This should
    ideally be configurable, and yet generics in package declarations
    require VHDL 2008 compliance, hence not done yet.
    
    Signed-off-by: Robert Jarzmik <robert.jarzmik@free.fr>

--- cpu_defs.vhd
+++ cpu_defs.vhd
@@ -6,7 +6,7 @@
 -- Author     : Robert Jarzmik  <robert.jarzmik@free.fr>
 -- Company    : 
 -- Created    : 2016-11-15
--- Last update: 2016-11-15
+-- Last update: 2016-12-06
 -- Platform   : 
 -- Standard   : VHDL'93/02
 -------------------------------------------------------------------------------
@@ -26,14 +26,19 @@ use ieee.std_logic_1164.all;
 
 package cpu_defs is
   constant REGISTER_WIDTH : integer := 32;
+  constant NB_REGISTERS_MAX : natural := 35;
 
+  attribute enum_encoding : string;
   type jump_type is (none, always, zero, non_zero, lesser_or_zero, greater, lesser, greater_or_zero);
+  attribute enum_encoding of jump_type : type is "sequential";
   type alu_op_type is (all_zero, add, substract, multiply, divide, log_and, log_or, log_xor, log_nor, slt);
+  attribute enum_encoding of alu_op_type : type is "sequential";
   type memory_op_type is (none, loadw, storew, load8, load8_signextend32, store8);
+  attribute enum_encoding of memory_op_type : type is "sequential";
 
   type register_port_type is record
     we   : std_logic;
-    idx  : natural;
+    idx  : natural range 0 to NB_REGISTERS_MAX;
     data : std_logic_vector(REGISTER_WIDTH - 1 downto 0);
   end record register_port_type;
 
